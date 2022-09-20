8ab0e777b26dc8a8b0d1fb647519a49f70b3b5fd
Micka73100 <Michael-73@live.Fr>
2022-09-20 15:19:59 +0200

    2heures20

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,11 +1,72 @@
 import "./new.scss"
+import Slidebar from '../../components/sidebar/Sidebar'
+import Navbar from '../../components/navbar/Navbar'
+import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined"
 
 const New = () => {
     return (
-        <div>
-            New
+        <div className="new">
+            <Slidebar />
+            <div className="newContainer">
+                <Navbar />
+                <div className="top">
+                    <h1>Add New User</h1>
+                </div>
+                <div className="bottom">
+                    <div className="left">
+                        <img src="https://images.pexels.com/photos/995978/pexels-photo-995978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=""></img>
+                    </div>
+                    <div className="right">
+                        <form>
+                            <div className="formInput">
+                                <label htmlFor="file">Image: <DriveFolderUploadOutlinedIcon /></label>
+                                <input type='file'id="file" style={{display: "none"}}/>
+                            </div>
+
+                            <div className="formInput">
+                                <label>Username</label>
+                                <input type='text' placeholder="John_Doe"/>
+                            </div>
+
+                            <div className="formInput">
+                                <label>Name and surname</label>
+                                <input type='text' placeholder="John Doe"/>
+                            </div>
+
+                            <div className="formInput">
+                                <label>Email</label>
+                                <input type='email' placeholder="John_Doe@gmail.com"/>
+                            </div>
+
+                            <div className="formInput">
+                                <label>Phone</label>
+                                <input type='text' placeholder="+33 607535627"/>
+                            </div>
+
+                            <div className="formInput">
+                                <label>Password</label>
+                                <input type='password'/>
+                            </div>
+
+                            <div className="formInput">
+                                <label>Adress</label>
+                                <input type='text' placeholder="1516 route de l'albanais Gresy-sur-aix"/>
+                            </div>
+
+                            <div className="formInput">
+                                <label>Pays</label>
+                                <input type='text' placeholder="France"/>
+                            </div>
+                            <button>Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
 
 export default New;
+
+
+//https://www.youtube.com/watch?v=yKV1IGahXqA&t=5891s 2h20
--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,6 +1,8 @@
 import './single.scss'
 import Sidebar from '../../components/sidebar/Sidebar'
 import Navbar from '../../components/navbar/Navbar'
+import Chart from '../../components/chart/Chart'
+import List from '../../components/table/Table'
 
 const Single = () => {
     return (
@@ -11,22 +13,45 @@ const Single = () => {
 
                 <div className='top'>
                     <div className='left'>
+                      <div className='editButton'>Edit</div>
                         <h1 className='title'>Information</h1>
-                        {/* <div className='item'>
-                            <img 
-                            src="https://images.pexels.com/photos/13445893/pexels-photo-13445893.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" 
-                            alt="" 
-                            className="itemImg"
-                            />
-                        </div> */}
+                            <div className='item'>
+                                <img 
+                                src="https://images.pexels.com/photos/13445893/pexels-photo-13445893.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" 
+                                alt="" 
+                                className="itemImg"
+                                />
+                                <div className="details">
+                                    <h1 className='itemTitle'>Jane Doe</h1>
+                                    <div className='detailItem'>
+                                        <span className='itemKey'>Email:</span>
+                                        <span className='itemValue'>Janedoe@gmail.com</span>
+                                    </div>
+                                    <div className='detailItem'>
+                                        <span className='itemKey'>Phone:</span>
+                                        <span className='itemValue'>+33607535627</span>
+                                    </div>
+                                    <div className='detailItem'>
+                                        <span className='itemKey'>Adresse:</span>
+                                        <span className='itemValue'>1516 r. de l'albanais Gresy-sur-aix</span>
+                                    </div>
+                                    <div className='detailItem'>
+                                        <span className='itemKey'>Pays:</span>
+                                        <span className='itemValue'>France</span>
+                                    </div>
+                                </div>
+                            </div>
                     </div>
 
                     <div className='right'>
-
+                        <Chart aspect={3/1} title="User Spending ( Last 6 Months )"/>
                     </div>
                 </div>
 
-                <div className='bottom'></div>
+                <div className='bottom'>
+                <h1 className='title'>Last Transactions</h1>
+                    <List />
+                </div>
             </div>
         </div>
     );
@@ -35,4 +60,4 @@ const Single = () => {
 export default Single;
 
 
-//https://www.youtube.com/watch?v=yKV1IGahXqA&t=5891s    1:58   
+//https://www.youtube.com/watch?v=yKV1IGahXqA&t=5891s   2H08
